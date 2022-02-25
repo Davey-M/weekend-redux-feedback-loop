bae0eb1809cc9ac1e1dde0557db7a717ba56e7c1
Davey-M <davey.meuer@gmail.com>
2022-02-25 16:52:13 -0600

    format docs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,14 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1 className='App-title'>Feedback!</h1>
+				<h4>Don't forget it!</h4>
+			</header>
+		</div>
+	);
 }
 
 export default App;
