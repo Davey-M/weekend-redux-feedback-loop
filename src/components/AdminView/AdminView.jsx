ee4508e9f191ba510443920fa4d92cf8532644d6
Davey-M <davey.meuer@gmail.com>
2022-02-26 01:59:46 -0600

    add table to admin view

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -1,5 +1,12 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import {
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+} from '@mui/material';
 
 function AdminView() {
 	const [feedback, setFeedback] = useState([]);
@@ -20,7 +27,44 @@ function AdminView() {
 	}, []);
 
 	console.log(feedback);
-	return <div className='view-wrapper'></div>;
+	return (
+		<div className='view-wrapper admin-wrapper'>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>
+							<b>Feeling</b>
+						</TableCell>
+						<TableCell>
+							<b>Understanding</b>
+						</TableCell>
+						<TableCell>
+							<b>Support</b>
+						</TableCell>
+						<TableCell>
+							<b>Comments</b>
+						</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{feedback.map((item) => {
+						const { id, feeling, understanding, support, comments } = item;
+						return (
+							<TableRow key={id}>
+								<TableCell>{feeling}</TableCell>
+								<TableCell>{understanding}</TableCell>
+								<TableCell>{support}</TableCell>
+								<TableCell>
+									{comments.substring(0, 20)}
+									{comments.length > 20 && '...'}
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</div>
+	);
 }
 
 export default AdminView;
