6ee6a3fbe913707c1394fde00276c34b57dd214f
Davey-M <davey.meuer@gmail.com>
2022-02-26 01:23:11 -0600

    add inserting into database

--- server/routers/feedback.router.js
+++ server/routers/feedback.router.js
@@ -6,7 +6,29 @@ const pool = require('../modules/pool');
 router.post('/', (req, res) => {
     console.log(req.body);
 
-    res.sendStatus(201);
+    const sqlText = `
+        INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+        VALUES
+            ($1, $2, $3, $4);
+    `
+
+    const { feelingStore, understandingStore, supportStore, commentsStore } = req.body;
+    const sqlOptions = [
+        feelingStore,
+        understandingStore,
+        supportStore,
+        commentsStore,
+    ]
+
+    pool.query(sqlText, sqlOptions)
+        .then(response => {
+            res.sendStatus(201);
+        })
+        .catch(err => {
+            console.error('Error posing to database', err);
+            res.sendStatus(500);
+        })
+
 })
 
 module.exports = router;
