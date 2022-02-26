fb7d9327d60a2483c0d2030525d1064c422de4a1
Davey-M <davey.meuer@gmail.com>
2022-02-26 01:45:52 -0600

    add axios request

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -1,4 +1,23 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function AdminView() {
+	const [feedback, setFeedback] = useState([]);
+
+	const getFeedback = () => {
+		axios
+			.get('/feedback')
+			.then((response) => {
+				setFeedback(response.data);
+			})
+			.catch((err) => {
+				console.error('Error getting feedback in AdminView', err);
+			});
+	};
+
+	useEffect(() => {}, []);
+
+	console.log(feedback);
 	return <div className='view-wrapper'></div>;
 }
 
