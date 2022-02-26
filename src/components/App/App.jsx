f55635fe0fcd292212c2acce239bfe5074561d21
Davey-M <davey.meuer@gmail.com>
2022-02-25 19:45:23 -0600

    add mui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,42 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+// mui imports
+import { Paper, Stepper, Step, StepLabel } from '@mui/material';
+
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h1 className='App-title'>Feedback!</h1>
-				<h4>Don't forget it!</h4>
+				<Paper>
+					<div className='header-paper'>
+						<h1>Feedback!</h1>
+						<p>Don't forget it!</p>
+					</div>
+				</Paper>
 			</header>
+			<main></main>
+			<footer className='App-footer'>
+				<Paper className='header-paper'>
+					<Stepper activeStep={0}>
+						<Step>
+							<StepLabel>Feelings</StepLabel>
+						</Step>
+						<Step>
+							<StepLabel>Understanding</StepLabel>
+						</Step>
+						<Step>
+							<StepLabel>Support</StepLabel>
+						</Step>
+						<Step>
+							<StepLabel>Comments</StepLabel>
+						</Step>
+						<Step>
+							<StepLabel>Review</StepLabel>
+						</Step>
+					</Stepper>
+				</Paper>
+			</footer>
 		</div>
 	);
 }
