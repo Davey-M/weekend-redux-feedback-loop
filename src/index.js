4b82c44b29c81f861baefd81c3e4d1483fee7864
Davey-M <davey.meuer@gmail.com>
2022-02-25 21:15:10 -0600

    add reducer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux imports
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+
+const test = (state = 0, action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        test,
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
