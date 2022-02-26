fe864075ca46b6c53d4d266cdea161b048d5fb06
Davey-M <davey.meuer@gmail.com>
2022-02-25 20:32:57 -0600

    add styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,40 +5,59 @@ import './App.css';
 // mui imports
 import { Paper, Stepper, Step, StepLabel } from '@mui/material';
 
+// router imports
+import {
+	HashRouter as Router,
+	Route,
+	Switch,
+	useHistory,
+} from 'react-router-dom';
+
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<Paper>
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					{/* <Paper> */}
 					<div className='header-paper'>
 						<h1>Feedback!</h1>
 						<p>Don't forget it!</p>
 					</div>
-				</Paper>
-			</header>
-			<main></main>
-			<footer className='App-footer'>
-				<Paper className='header-paper'>
-					<Stepper activeStep={0}>
-						<Step>
-							<StepLabel>Feelings</StepLabel>
-						</Step>
-						<Step>
-							<StepLabel>Understanding</StepLabel>
-						</Step>
-						<Step>
-							<StepLabel>Support</StepLabel>
-						</Step>
-						<Step>
-							<StepLabel>Comments</StepLabel>
-						</Step>
-						<Step>
-							<StepLabel>Review</StepLabel>
-						</Step>
-					</Stepper>
-				</Paper>
-			</footer>
-		</div>
+					{/* </Paper> */}
+				</header>
+				<main>
+					<Route path='/' exact>
+						Hi
+					</Route>
+					<Route path='/hello' exact>
+						Hello
+					</Route>
+				</main>
+				<footer className='App-footer'>
+					{/* <Paper className='header-paper'> */}
+					<div className='stepper-wrapper'>
+						<Stepper activeStep={0}>
+							<Step>
+								<StepLabel>Feelings</StepLabel>
+							</Step>
+							<Step>
+								<StepLabel>Understanding</StepLabel>
+							</Step>
+							<Step>
+								<StepLabel>Support</StepLabel>
+							</Step>
+							<Step>
+								<StepLabel>Comments</StepLabel>
+							</Step>
+							<Step>
+								<StepLabel>Review</StepLabel>
+							</Step>
+						</Stepper>
+					</div>
+					{/* </Paper> */}
+				</footer>
+			</div>
+		</Router>
 	);
 }
 
