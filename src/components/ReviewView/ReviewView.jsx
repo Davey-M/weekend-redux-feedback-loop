df158bb35f0094166e7bbd9de4244b136f5adead
Davey-M <davey.meuer@gmail.com>
2022-02-25 23:09:44 -0600

    style review view

--- src/components/ReviewView/ReviewView.jsx
+++ src/components/ReviewView/ReviewView.jsx
@@ -1,33 +1,46 @@
 import { useSelector } from 'react-redux';
-import { List, ListItem, ListItemText, Divider } from '@mui/material';
+import { List, ListItem, ListItemText, Divider, Button } from '@mui/material';
+import { useState } from 'react';
 
 function ReviewView() {
 	const store = useSelector((store) => store);
 
+	const [show, setShow] = useState(false);
+
 	console.log(store);
 	return (
-		<div>
-			<List>
-				<ListItem>
-					<ListItemText>Feeling: {store.feelingStore}</ListItemText>
-				</ListItem>
-				<Divider />
-				<ListItem>
-					<ListItemText>Understanding: {store.understandingStore}</ListItemText>
-				</ListItem>
-				<Divider />
-				<ListItem>
-					<ListItemText>Support: {store.supportStore}</ListItemText>
-				</ListItem>
-				<Divider />
-				<ListItem button>
-					<ListItemText>
-						Comments: {store.commentsStore.substring(0, 15)}
-						{store.commentsStore.length > 15 && '...'}
-					</ListItemText>
-				</ListItem>
-				<Divider />
-			</List>
+		<div className='review-wrapper'>
+			<div className='review-items'>
+				<List>
+					<ListItem>
+						<ListItemText>
+							<b>Feeling:</b> {store.feelingStore}
+						</ListItemText>
+					</ListItem>
+					<Divider />
+					<ListItem>
+						<ListItemText>
+							<b>Understanding:</b> {store.understandingStore}
+						</ListItemText>
+					</ListItem>
+					<Divider />
+					<ListItem>
+						<ListItemText>
+							<b>Support:</b> {store.supportStore}
+						</ListItemText>
+					</ListItem>
+					<Divider />
+					<ListItem button onClick={() => setShow(!show)}>
+						<ListItemText>
+							<b>Comments:</b> {store.commentsStore.substring(0, 40)}
+							{!show && store.commentsStore.length > 40 && '...'}
+							{show && store.commentsStore.substring(40)}
+						</ListItemText>
+					</ListItem>
+					<Divider />
+				</List>
+			</div>
+			<Button variant='contained'>Submit</Button>
 		</div>
 	);
 }
